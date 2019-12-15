890b6e4391edd0181d8a560e10128982558416db
TavH <tavherzlich26@gmail.com>
2019-12-15 23:17:23 +0200

    built header

---
+++ src/app/components/button_sets/guest-buttons/guest-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-buttons',
+  templateUrl: './guest-buttons.component.html',
+  styleUrls: ['./guest-buttons.component.css']
+})
+export class GuestButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
