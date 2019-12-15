26c4ab733728b297af20e6c3986ac43f74e7f8bc
TavH <tavherzlich26@gmail.com>
2019-12-16 00:22:42 +0200

    Added platform detection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HeaderDesktopComponent } from './components/desktop/header-desktop/header-desktop.component';
 import { GuestButtonsDesktopComponent } from './components/desktop/button_sets_desktop/guest-buttons-desktop/guest-buttons-desktop.component';
 import { ViewDesktopComponent } from './components/views/view-desktop/view-desktop.component';
+import { ViewMobileComponent } from './components/views/view-mobile/view-mobile.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { ViewDesktopComponent } from './components/views/view-desktop/view-deskt
     MainComponent,
     HeaderDesktopComponent,
     GuestButtonsDesktopComponent,
-    ViewDesktopComponent
+    ViewDesktopComponent,
+    ViewMobileComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-main',
+  providers: [DeviceDetectorService],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  currentDevice: string
+  
+  constructor(private deviceService: DeviceDetectorService) {
+    if (window.innerWidth > 800) {
+        this.currentDevice = 'desktop'
+    } else {
+        this.currentDevice = 'mobile'
+    }
+    
+    // if (deviceService.isDesktop()) {
+    //     this.currentDevice = 'desktop'
+    // }
 
+    // if (deviceService.isMobile()) {
+    //     this.currentDevice = 'mobile'
+    // }
+  }
+
+  
   ngOnInit() {
   }
 
