4c0baf8c99d6b638d402a8122b62062e5aa06246
TavH <tavherzlich26@gmail.com>
2019-12-27 00:40:23 +0200

    Improved css

--- src/app/components/mobile/button_sets_mobile/guest-buttons-mobile/guest-buttons-mobile.component.ts
+++ src/app/components/mobile/button_sets_mobile/guest-buttons-mobile/guest-buttons-mobile.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 @Component({
   selector: 'app-guest-buttons-mobile',
@@ -8,41 +7,50 @@ import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_c
 })
 export class GuestButtonsMobileComponent implements OnInit {
 
-    private isViewGlassesMenu: boolean;
-    private isViewLensesMenu: boolean;
-
+    
     constructor() { }
 
-    changeGlassesMenuVisibility() {
-        if (this.isViewGlassesMenu == true) {
-            this.isViewGlassesMenu = false
-        } else {
-            this.isViewLensesMenu = false
-            this.isViewGlassesMenu = true
-        }
+    changeSubMenuState(subMenuId) {
+        let subMenu = document.getElementById(subMenuId)
+        let subMenuVisibility = subMenu.style.visibility
+
+        switch (subMenuVisibility) {
+            case "hidden": this.viewSubMenu(subMenu)
+            break
+
+            case "visible": this.hideSubMenu(subMenu)
+            break
+
+            default: this.viewSubMenu(subMenu)
+        } 
     }
 
-    changeLensesMenuVisibility() {
-        if (this.isViewLensesMenu == true) {
-            this.isViewLensesMenu = false
-        } else {
-            this.isViewGlassesMenu = false
-            this.isViewLensesMenu = true
-        }
+    private viewSubMenu(subMenu) {
+        subMenu.style.visibility = "visible";
+        subMenu.style.maxHeight = "200px";
+
+        this.closeSubMenus(subMenu)
+    }
+
+    private hideSubMenu(subMenu) {
+        subMenu.style.visibility = "hidden";
+        subMenu.style.maxHeight = "0px";
+    }
+
+    closeSubMenus(excludedElement?) {
+        let subMenus = Array.from(document.getElementsByClassName("sub_menu"))
+
+        subMenus.forEach(subMenu => {
+            if (subMenu != excludedElement) {
+                this.hideSubMenu(subMenu)
+            }
+        });
     }
 
     closeMenu() {
         let menuToggler = document.getElementById("toggler")
 
-        this.isViewGlassesMenu = false
-        this.isViewLensesMenu = false
-
         menuToggler.click()
-    }
-
-    closeSubMenus() {
-        this.isViewGlassesMenu = false
-        this.isViewLensesMenu = false
     }
 
     ngOnInit() {
