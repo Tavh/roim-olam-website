c3f02d6ef92999478874d408afaa02302ac927ce
TavH <tavherzlich26@gmail.com>
2019-12-27 19:35:49 +0200

    Added image slider

--- src/app/components/mobile/home/home.component.ts
+++ src/app/components/mobile/home/home.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    public static readonly SLIDE_DIRECTION_RIGHT = "right"
+    public static readonly SLIDE_DIRECTION_LEFT = "left"
 
-  ngOnInit() {
-  }
+    private currentSlideIndex: number = 0
+    private images: Element[]
+    
+    ngOnInit() {
+        this.images = Array.from(document.getElementsByClassName("slide_image"))
+        this.changeSlider()
+    }
+    
+    constructor() {}
+    
+    changeSlider(direction?) {    
+        this.images.forEach(image => {
+            (image as HTMLElement).style.display = "none"
+        });
+    
+        if (direction == HomeComponent.SLIDE_DIRECTION_RIGHT 
+            && this.currentSlideIndex < this.images.length - 1) {
+                this.currentSlideIndex++    
+            }
+        
+        if (direction == HomeComponent.SLIDE_DIRECTION_LEFT
+            && this.currentSlideIndex > 0) {
+                this.currentSlideIndex--
+            }
+        
+        ((this.images[this.currentSlideIndex]) as HTMLElement).style.display = "block"
+    }
 
+    changeSliderToIndex(index) {
+        if (index < this.images.length && index >= 0) {
+            this.currentSlideIndex = index
+            this.changeSlider()
+        } 
+    }
 }
