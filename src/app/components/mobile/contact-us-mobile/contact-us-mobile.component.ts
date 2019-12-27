85364c56a3112764496a768b4994df371af919b6
TavH <tavherzlich26@gmail.com>
2019-12-27 22:42:27 +0200

    Added contact-us implementation

--- src/app/components/mobile/contact-us-mobile/contact-us-mobile.component.ts
+++ src/app/components/mobile/contact-us-mobile/contact-us-mobile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDetails } from 'src/app/shared/data/user.model';
+import { WebsiteStaticContentService } from 'src/app/shared/services/http-constructors/website-static-content.service';
+import { SessionStorageManager } from 'src/app/shared/session-storage-manager';
+import { WebsitePagePart } from 'src/app/shared/data/enums/webiste-page-part';
+import { WebsitePage } from 'src/app/shared/data/enums/website-page';
+import { TextWrapper } from 'src/app/shared/data/text-wrapper';
 
 @Component({
   selector: 'app-contact-us-mobile',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsMobileComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    private currentHeader: string
+    private currentContent: string
+  
+    private sessionUserDetails: UserDetails
+  
+    constructor(private websiteStaticContentService: WebsiteStaticContentService) {
+      SessionStorageManager.initializeSessionStorageCurrentUserData()
+      this.sessionUserDetails = SessionStorageManager.getSessionStorageUserDetails()
+  
+      this.readPageText(WebsitePagePart.HEADER.toString())
+      this.readPageText(WebsitePagePart.CONTENT.toString())
+    }
+  
+    ngOnInit() {
+    }
+  
+    readPageText(websitePagePart) {
+      const observable =  this.websiteStaticContentService.readStaticContent(WebsitePage.CONTACT_US, websitePagePart)
+  
+      observable.subscribe(
+        res => {
+          if (res.status == 200) {
+            if (websitePagePart == WebsitePagePart.HEADER.toString()) {
+              this.currentHeader = res.body.text
+            } else if (websitePagePart == WebsitePagePart.CONTENT.toString()) {
+              this.currentContent = res.body.text
+            }
+          }
+        },
+        err => {
+          console.log(err)
+        })
+    }
 }
