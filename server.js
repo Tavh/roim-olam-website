00f8e49843a46e72618ca99e51e79001ffd008b7
tav <silvermill123@gmail.com>
2020-05-07 01:20:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist/angular-on-heroku'));
 
-app.listen(PORT || 8080, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  })
+// Path location strategy
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+        '/dist/roim-olam-website/index.html'));
+    });
 
-// PathLocationStrategy
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/roim-olam-website/index.html'))
-})
-
-console.log('Console listening!')
+app.listen(process.env.PORT || 8080);
