277dda47c9d7b97bc0322eac571e3a90fa5c2ff3
tav <silvermill123@gmail.com>
2020-05-07 01:13:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT
 
-app.use(express.static(_dirname + '/dist'))
+app.use(express.static(__dirname + '/dist'))
 
 app.listen(PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
@@ -11,7 +11,7 @@ app.listen(PORT || 8080, function(){
 
 // PathLocationStrategy
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname + '/dist/roim-olam-website/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/roim-olam-website/index.html'))
 })
 
 console.log('Console listening!')
