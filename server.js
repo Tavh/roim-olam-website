67700db8b8579137b05eacd5fcfaf345a87e888e
tav <silvermill123@gmail.com>
2020-05-07 00:53:26 +0300

    heroku deploy changes
    Changed files hierarchy and config

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const defaultPort = 8080
+const PORT = process.env.PORT
 
 app.use(express.static(_dirname + '/dist'))
-app.listen(process.env.PORT || defaultPort)
+
+app.listen(PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  })
 
 // PathLocationStrategy
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname + '/dist/index.html'))
+    res.sendFile(path.join(_dirname + '/dist/roim-olam-website/index.html'))
 })
 
 console.log('Console listening!')
