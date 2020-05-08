e586185f6363d6ef858647eb4f7c08feca233366
tav <silvermill123@gmail.com>
2020-05-08 12:41:20 +0300

    Update server-constants.model.ts

--- src/app/shared/constants/server-constants.model.ts
+++ src/app/shared/constants/server-constants.model.ts
@@ -1,3 +1,3 @@
 export class ServerConstants {
-    public static readonly HOST_AND_PORT = "https://roim-olam-server.herokuapp.com"
+    public static readonly HOST_AND_PORT = "http://roimolamserver-env.eba-dy8wbr2h.eu-central-1.elasticbeanstalk.com"
 }
