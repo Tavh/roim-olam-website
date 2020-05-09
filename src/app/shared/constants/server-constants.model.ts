3a75bc589b5cef302f090f0cc2bdd96768c334e1
tav <silvermill123@gmail.com>
2020-05-09 18:09:46 +0300

    Update server-constants.model.ts

--- src/app/shared/constants/server-constants.model.ts
+++ src/app/shared/constants/server-constants.model.ts
@@ -1,5 +1,5 @@
 export class ServerConstants {
-    public static readonly HOST_AND_PORT = "http://http://roimolamserver-env.eba-dy8wbr2h.eu-central-1.elasticbeanstalk.com/:5000"
+    public static readonly HOST_AND_PORT = "http://roimolamserver-env.eba-dy8wbr2h.eu-central-1.elasticbeanstalk.com:5000"
     public static readonly ERROR_MESSAGE_HEADER = "errorMessage"
 
 }
