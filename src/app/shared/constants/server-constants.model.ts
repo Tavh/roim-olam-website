5b14d1ab0ed27d9f5cee2cdbc0efecf77d4fc4ee
Tavh <44731477+Tavh@users.noreply.github.com>
2020-05-13 16:35:20 +0300

    fixed up font jump in buttons

--- src/app/shared/constants/server-constants.model.ts
+++ src/app/shared/constants/server-constants.model.ts
@@ -1,6 +1,6 @@
 export class ServerConstants {
-    // public static readonly HOST_AND_PORT = "http://roimolamserver-env.eba-dy8wbr2h.eu-central-1.elasticbeanstalk.com:5000"
-    public static readonly HOST_AND_PORT = "http://localhost:5000"
+    public static readonly HOST_AND_PORT = "http://roimolamserver-env.eba-dy8wbr2h.eu-central-1.elasticbeanstalk.com:5000"
+    // public static readonly HOST_AND_PORT = "http://localhost:5000"
     public static readonly ERROR_MESSAGE_HEADER = "errorMessage"
 
     public static readonly HTTP_GET_SUCCESS_STATUS = 200
