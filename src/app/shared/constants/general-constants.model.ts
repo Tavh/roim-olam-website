b81ebd940c355f3f84a49418dcdf6ed666f14b33
tav <silvermill123@gmail.com>
2020-05-20 22:03:26 +0300

    changed max file size

--- src/app/shared/constants/general-constants.model.ts
+++ src/app/shared/constants/general-constants.model.ts
@@ -1,7 +1,7 @@
 export class GeneralConstants {
     public static readonly JPG_POSTFIX = ".jpg"  
     public static readonly JPEG_POSTFIX = ".jpeg"
-    public static readonly MAX_PHOTO_SIZE = 1500000
+    public static readonly MAX_PHOTO_SIZE = 10000000
     public static readonly MEGABYTE_FACTOR = 1000000
 
     public static readonly CURRENT_CATALOG_TYPE_KEY = "CurrentCatalogType"
